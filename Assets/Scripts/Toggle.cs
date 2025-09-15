403afa7c3f17e73feeb4337618205afad195c7de
DylanColeman <000914931@mohawkcollege.ca>
2025-09-15 12:11:17 -0400

    Minor Changes
    Made it so text only appears when you get close enough

---
+++ Assets/Scripts/Toggle.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Toggle : MonoBehaviour
+{
+    public TextMeshProUGUI text1;
+    public TextMeshProUGUI text2;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        text1.enabled = false;
+        text2.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            text1.enabled = true;
+            text2.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            text1.enabled = false;
+            text2.enabled = false;
+        }
+    }
+}
