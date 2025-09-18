96d5d38df542fa594cba204f7935f15e13cb9aac
DylanColeman <000914931@mohawkcollege.ca>
2025-09-18 16:31:18 -0400

    Basic Map Addition

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject[] objects;
+    private bool nearPlayer = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nearPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < objects.Length; i++) // For every object in that
+            {
+                if (objects[i] != null)
+                {
+                    Destroy(objects[i]);
+                    objects[i] = null; // clears the reference
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("player"))
+        {
+            nearPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            nearPlayer = false;
+        }
+    }
+}
